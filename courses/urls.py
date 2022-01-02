928dae6bcf07ab6268d6a47ce965c2ba681cb365
unknown <osamamuhammed555@gmail.com>
2022-01-02 14:19:04 +0300

    fix footer

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CourseDetailView
+from .views import CourseDetailView, CourseListView
 
 urlpatterns = [
+    path('',CourseListView.as_view(), name='courses_list' ),
     path('course/<int:pk>/',CourseDetailView.as_view(), name='course_detail' ),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Course
 # Create your views here.
 
@@ -9,3 +9,10 @@ from .models import Course
 class CourseDetailView(DetailView):
     model = Course
     template_name = "courses/course_detail.html"
+
+
+
+class CourseListView(ListView):
+    model = Course
+    template_name = "courses/courses_list.html"
+    
