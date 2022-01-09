7542c380f360b3590bcb7bfe3c8f744515e475dc
unknown <osamamuhammed555@gmail.com>
2022-01-09 13:16:06 +0300

    ntc

--- articles/views.py
+++ articles/views.py
@@ -13,25 +13,18 @@ def index(request):
     categories = Category.objects.all()
     paginator = Paginator(articles, 5)
 
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     # ? context data for binding
     context = {
         "categories": categories,
-        'page_obj': page_obj,
-
+        "page_obj": page_obj,
     }
 
-
     return render(request, "articles/articles_list.html", context=context)
-
-
-
-
 
 
 class ArticleDetailView(DetailView):
     model = Article
     template_name = "articles/article_detail.html"
-
