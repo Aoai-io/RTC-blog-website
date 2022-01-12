d639a0bb651073e790cd3376714092b3f19982e0
unknown <osamamuhammed555@gmail.com>
2022-01-12 16:21:40 +0300

    nc

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,5 @@ urlpatterns = [
     path('instructors/', include('instructors.urls')),
     path('courses/', include('courses.urls')),
 ] 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
