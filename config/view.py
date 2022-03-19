0ed05d2badcc7753b5616e41da0d26de7e296523
osamamoh890 <osamamuhammed555@gmail.com>
2022-03-19 13:56:39 +0300

    nc

--- config/view.py
+++ config/view.py
@@ -35,8 +35,6 @@ def HomePageView(request):
 
 def Deploy(request):
     
-    
-
     os.system('touch tmp.deploy.txt')
 
     file_object = open('tmp.deploy.txt', 'a')
@@ -80,6 +78,6 @@ def Deploy(request):
 
     file_name = f'deploy-log-{datetime.datetime.now()}.txt'
 
-    os.system(f'cp tmp.deploy.txt ../logs/{file_name}')
+    os.system(f'cp /home/osama/RTC-blog-website/tmp.deploy.txt /home/osama/logs/{file_name}')
     # os.system(f'rm tmp.deploy.txt')
     return HttpResponse("<h1>Deploy Successful</h1>")
