afe1a691c9c5e6df9b98548e652150a815e7f82f
osamamoh890 <osamamuhammed555@gmail.com>
2022-03-21 11:58:50 +0300

    nc

--- config/view.py
+++ config/view.py
@@ -40,26 +40,26 @@ def HomePageView(request):
 
 def Deploy(request):
 
-    os.system("touch tmp.deploy.txt")
+    # os.system("touch tmp.deploy.txt")
 
-    file_object = open("tmp.deploy.txt", "a")
-    file_object.write(f"{datetime.datetime.now()}\n")
-    file_object.write("###################################################\n")
-    file_object.close()
+    # file_object = open("tmp.deploy.txt", "a")
+    # file_object.write(f"{datetime.datetime.now()}\n")
+    # file_object.write("###################################################\n")
+    # file_object.close()
 
-    os.system(env("DEFAULT_GIT_QUERY") + " >> tmp.deploy.txt 2>&1 &")
+    # os.system(env("DEFAULT_GIT_QUERY") + " >> tmp.deploy.txt 2>&1 &")
 
-    os.system("source ../venv/bin/activate >> tmp.deploy.txt 2>&1 &")
+    # os.system("source ../venv/bin/activate >> tmp.deploy.txt 2>&1 &")
 
-    os.system("pip install -r requirements.txt >> tmp.deploy.txt 2>&1 &")
+    # os.system("pip install -r requirements.txt >> tmp.deploy.txt 2>&1 &")
 
-    os.system("python manage.py migrate >> tmp.deploy.txt 2>&1 &")
+    # os.system("python manage.py migrate >> tmp.deploy.txt 2>&1 &")
 
-    os.system("echo Omar@wolf.9803 | sudo -S systemctl restart gunicorn.service 2>&1 &")
-    os.system("echo Omar@wolf.9803 | sudo -S systemctl restart nginx.service 2>&1 &")
+    # os.system("echo Omar@wolf.9803 | sudo -S systemctl restart gunicorn.service 2>&1 &")
+    # os.system("echo Omar@wolf.9803 | sudo -S systemctl restart nginx.service 2>&1 &")
 
-    os.system(
-        "cp /home/osama/RTC-blog-website/tmp.deploy.txt /home/osama/logs/deploy.txt 2>&1 &"
-    )
-    os.system("rm tmp.deploy.txt 2>&1 &")
+    # os.system(
+    #     "cp /home/osama/RTC-blog-website/tmp.deploy.txt /home/osama/logs/deploy.txt 2>&1 &"
+    # )
+    # os.system("rm tmp.deploy.txt 2>&1 &")
     return HttpResponse(os.system('cat note.txt'))
