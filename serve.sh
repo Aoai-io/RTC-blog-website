6f4aafcd5d07d2e7900a2117ecb255f707c1a707
osamamoh890 <osamamuhammed555@gmail.com>
2022-03-21 12:17:58 +0300

    nc

--- serve.sh
+++ serve.sh
@@ -3,14 +3,16 @@
 echo $(cd /home/osama/)
 echo $(source /venv/bin/activate)
 echo $(pwd)
-echo $(cd /home/osama/RTC-blog-website && touch tmp.deploy.txt)
+echo $(cd /home/osama/RTC-blog-website)
 # echo $(touch /home/osama/RTC-blog-website/tmp.deploy.txt)
 echo $(git pull origin main)
 echo $(date)
 echo $(pwd)
 
 echo $(pip install -r requirements.txt)
-echo $(python manage.py migrate)
+echo $(python manage.py makemigration && python manage.py migrate)
+echo $(python manage.py collectstatic --noinput)
+
 echo $(echo Omar@wolf.9803 | sudo -S systemctl restart gunicorn.service)
 echo $(echo Omar@wolf.9803 | sudo -S systemctl restart nginx.service)
 echo $(cp /home/osama/RTC-blog-website/tmp.deploy.txt /home/osama/logs/deploy.txt)
