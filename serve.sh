70130e0d08c2e007f7d81d292cfa6ff41036d0e0
osamamoh890 <osamamuhammed555@gmail.com>
2022-03-21 12:12:42 +0300

    add nc

--- serve.sh
+++ serve.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo $(touch tmp.deploy.txt)
-echo $(cd /home/osama/RTC-blog-website)
+echo $(cd /home/osama/)
+echo $(source /venv/bin/activate)
+echo $(pwd)
+echo $(cd /home/osama/RTC-blog-website && touch tmp.deploy.txt)
+# echo $(touch /home/osama/RTC-blog-website/tmp.deploy.txt)
+echo $(git pull origin main)
 echo $(date)
 echo $(pwd)
-echo $(git pull origin main)
-echo $(source ../venv/bin/activate)
 
 echo $(pip install -r requirements.txt)
 echo $(python manage.py migrate)
-echo $($(echo) Omar@wolf.9803 | sudo -S systemctl restart gunicorn.service)
-echo $($(echo) Omar@wolf.9803 | sudo -S systemctl restart nginx.service)
+echo $(echo Omar@wolf.9803 | sudo -S systemctl restart gunicorn.service)
+echo $(echo Omar@wolf.9803 | sudo -S systemctl restart nginx.service)
 echo $(cp /home/osama/RTC-blog-website/tmp.deploy.txt /home/osama/logs/deploy.txt)
 echo $(rm tmp.deploy.txt)
